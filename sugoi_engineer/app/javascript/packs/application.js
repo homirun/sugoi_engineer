cedab43d0d18dd329c3fd954fa9606f8e051aac4
homirun <hatunemiku.moco@gmail.com>
2021-08-21 21:52:27 +0900

    react導入

--- sugoi_engineer/app/javascript/packs/application.js
+++ sugoi_engineer/app/javascript/packs/application.js
@@ -1,13 +1,21 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
+import ReactDOM from "react-dom"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
+import "bootstrap"
+import App from "components"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("DOMContentLoaded", () => {
+    ReactDOM.render(<App />, document.getElementById("app"))
+})
+
+// turbolinksによる画面遷移時は DOMContentLoaded ではなく turbolinks:load イベントが発火する
+document.addEventListener("turbolinks:load", () => {
+    ReactDOM.render(<App />, document.getElementById("app"))
+})
