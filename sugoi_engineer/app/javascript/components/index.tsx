222aafe5a4882259792cd890c058a38e0f1802e9
homirun <hatunemiku.moco@gmail.com>
2021-08-22 21:56:52 +0900

    フロントエンドとりあえず一区切り

--- sugoi_engineer/app/javascript/components/index.tsx
+++ sugoi_engineer/app/javascript/components/index.tsx
@@ -1,45 +1,81 @@
 import * as React from "react";
-import { useState } from "react"
-
+import {useState} from "react";
+import axios from "axios";
 
 const Top = (props) => {
     const [isVisibleForm, setIsVisibleForm] = useState(false);
-    const [engineerName, setEngineerName] = useState("")
-    const [isStartAnimation, setIsStartAnimation] = useState(false)
+    const [engineerName, setEngineerName] = useState("");
+    const [isStartAnimation, setIsStartAnimation] = useState(false);
 
     const handleClickVisibilityFormToggleSwitch = () => {
-        setIsStartAnimation(!isStartAnimation)
-        setIsVisibleForm(!isVisibleForm)
+        setIsStartAnimation(!isStartAnimation);
+        setIsVisibleForm(!isVisibleForm);
     }
 
     const handleChangeEngineerName = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setEngineerName(event.target.value)
+        setEngineerName(event.target.value);
+    }
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        if (engineerName === "") {
+            alert('GitHubのIDを入力してください');
+            return;
+        }
+
+        try {
+            const data = new FormData();
+            data.append("engineer[name]", engineerName);
+
+            const res = await axios.post("/engineers.json", data);
+            setEngineerName("");
+            alert("https://sugoi.engineer/" + engineerName + "/ の作成に成功しました");
+
+        } catch (e) {
+
+        }
     }
 
     return (
         <>
             <div id="sugoi-content-container">
-                <div id="sugoi-title" className={isStartAnimation ? 'start-animation': ''}>
-                    <h1><span className="sugoi-rainbow" onClick={handleClickVisibilityFormToggleSwitch}>sugoi</span>.engineer</h1>
+                <div id="sugoi-title" className={isStartAnimation ? 'start-animation' : ''}>
+                    <h1><span className="sugoi-rainbow" onClick={handleClickVisibilityFormToggleSwitch}>sugoi</span>.engineer
+                    </h1>
                 </div>
-                { isVisibleForm &&
-                <form id="sugoi-form">
-                    /
-                    <input type="text" placeholder="Name!!" value={engineerName} onChange={handleChangeEngineerName} />
-                    <input type="submit" value="Post" className="btn btn-primary"/>
-                </form> }
+                {isVisibleForm &&
+                <>
+                    <form id="sugoi-form-engineer" className={isStartAnimation ? 'start-animation' : ''}>
+                        /
+                        <input type="text" placeholder="GitHub ID" value={engineerName}
+                               onChange={handleChangeEngineerName}/>
+                    </form>
+                    <div id="sugoi-message" className={isStartAnimation ? 'start-animation' : ''}>
+                        <span className="sugoi-rainbow">sugoi.engineer/{engineerName}</span>からgithub.com/{engineerName}へリダイレクト出来るようになります。
+                        <br/>(リンクは不定期に消える可能性があります)
+                    </div>
+                    <div id="sugoi-form-submit" onClick={handleSubmit}
+                         className={isStartAnimation ? 'start-animation' : ''}>
+                        <input type="submit" value="登録"/>
+                    </div>
+
+                </>
+                }
+
+
             </div>
         </>
     );
 }
 
-
-const App = () => {
+const App = () =>
+{
     return (
         <Top
 
         ></Top>
     );
-};
+}
+;
 
 export default App;
